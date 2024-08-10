2e8e55ca300ea7564f24b134af31dc66313de8e8
Murylo Gomes Alves <muryloalves15@gmail.com>
2024-08-10 20:12:18 -0300

    alterando a idade de 18 para 20

--- Projeto/projeto.py
+++ Projeto/projeto.py
@@ -1,7 +1,7 @@
 nome_do_usuario = str(input('Digite seu nome'))
 idade_do_usuario = int(input('Digite sua idade'))
 
-if idade_do_usuario < 18:
+if idade_do_usuario < 20:
     print(f'{nome_do_usuario} você não tem idade suficiente.')
-elif idade_do_usuario >= 18:
+elif idade_do_usuario >= 20:
     print(f'{nome_do_usuario} parabens, voçê tem idade suficiente!')
